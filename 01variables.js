d09c1febab6bb9fc87c43de558c60b1f5b5a5bd0
Prince Subedi <subediprince05@gmail.com>
2025-09-20 22:53:24 +0545

    Basic Of JavaScript

---
+++ 01variables.js
@@ -0,0 +1,7 @@
+console.log("Hello Guyss!!!")
+
+let a=69
+console.log(a)
+
+let b="Harry"
+console.log(b)
--- start.js
+++ start.js
@@ -4,5 +4,5 @@ function addno(a,b,c){
     return a+b+c
 }
 
-let c=addno(5,6,7);
+let c=addno(5,6,7)
 console.log(c)
